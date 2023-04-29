550e4dd18ff238e1dcce4c180cea7ff42e613942
Drag-yong <ypark272@wisc.edu>
2023-04-29 00:06:02 -0500

    sth

--- Kaggle_raw_dataset/img_to_csv.py
+++ Kaggle_raw_dataset/img_to_csv.py
@@ -45,7 +45,7 @@ def get_image_pixels():
         root_dir = './' + e
         for dirpath, dirnames, filenames in os.walk(root_dir):
             for filename in filenames:
-                if filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg'):
+                if filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg') or filename.lower().endswith('.png'):
                     random_number = random.randint(0, 99)
                     if 0 <= random_number and random_number < 60:
                         usage = 'train'
