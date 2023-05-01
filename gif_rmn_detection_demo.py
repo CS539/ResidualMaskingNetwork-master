ca835cc77c8cf48e52560907e3665e55695eb3cf
Janice-ahn <jihyun.ahn.ai@gmail.com>
2023-05-01 11:45:02 -0500

    modify demo file

--- gif_rmn_detection_demo.py
+++ gif_rmn_detection_demo.py
@@ -7,7 +7,7 @@ m = RMN()
 # import save_gif_to_imgs as sgti
 
 # ## save_gif_to_imgs(csv_file_path_without_name: str, csv_file_name: str, save_filepaths: str):
-# sgti.save_gif_to_imgs('./GIF_input/', 'faces.csv', './GIF_output/gif_to_jpgs_results/')
+# sgti.save_gif_to_imgs('./GIF_input/temp_dir/', 'faces.csv', './GIF_output/gif_to_jpgs_results/')
 
 # ## Step 3: call detect_emotions_and_store_n_list function
 # #  detect_emotions_and_store_n_list(save_filepaths which has been used above : str, gif_file_name: str):
